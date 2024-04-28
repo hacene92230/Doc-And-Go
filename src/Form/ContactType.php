4b5f1b65644c47b5e652ffe3d3cd94be1d7c103d
--user.email <hacenedu92@live.fr>
2024-04-28 20:00:45 +0200

    implementation of the planning system in particular with form collections and improvement of the appointment system but not finalization and implementation of the contact system.

---
+++ src/Form/ContactType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add ('name', TextType::class, [
+            'label'=> "Votre prénom"
+        ])
+        
+        ->add('email',EmailType::class, [
+            'label'=> "Votre adresse email"
+        ])
+
+        ->add('phone', TelType::class, [
+            'label' => "Saisir votre numéro de téléphone"
+        ])
+
+        ->add('subject',TextType::class, [
+            'label' => "Sujet de votre contact"
+        ])
+
+        ->add('content',TextareaType::class, [
+            'label' => "Contenu de votre message"
+        ] )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
